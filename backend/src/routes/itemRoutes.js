cfa6bcf0729c62c4fb9578d82951b5f98466654e
Anthony <anthonyguilherne@gmail.com>
2026-05-24 16:52:32 -0300

    Update itemRoutes.js

--- backend/src/routes/itemRoutes.js
+++ backend/src/routes/itemRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { create, list, update, deleteItem } = require('../controllers/itemController');
+const { create, list, getById, listByUser, update, deleteItem } = require('../controllers/itemController');
 const { autenticar } = require('../middlewares/authMiddleware');
 
 router.post('/', autenticar, create);
 router.get('/', autenticar, list);
+router.get('/meus', autenticar, listByUser);
+router.get('/:id', autenticar, getById);
 router.put('/:id', autenticar, update);
 router.delete('/:id', autenticar, deleteItem);
 
