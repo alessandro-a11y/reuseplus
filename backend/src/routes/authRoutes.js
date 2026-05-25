a1e31c0f950a2e3403e5105df6ff830967534a8b
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-25 08:37:12 -0300

    feat: adiciona rotas /me, /users, /users/me protegidas com JWT

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { cadastrar, entrar } = require('../controllers/authController');
+const { cadastrar, entrar, me, listar, atualizar, deletar } = require('../controllers/authController');
+const { autenticar } = require('../middlewares/authMiddleware');
 
 router.post('/register', cadastrar);
 router.post('/login', entrar);
+router.get('/me', autenticar, me);
+router.get('/users', autenticar, listar);
+router.put('/users/me', autenticar, atualizar);
+router.delete('/users/me', autenticar, deletar);
 
 module.exports = router;
